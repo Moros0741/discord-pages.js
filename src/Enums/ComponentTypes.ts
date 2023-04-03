e3633431810d24b9896f9df2d56b63027bc39408
Moros <71281300+Moros0741@users.noreply.github.com>
2023-04-03 18:08:08 -0400

    move ComponentTypes to its own file

---
+++ src/Enums/ComponentTypes.ts
@@ -0,0 +1,4 @@
+export enum ComponentTypes {
+  Button,
+  SelectMenu,
+}
--- src/Enums/PaginatorTypes.ts
+++ src/Enums/PaginatorTypes.ts
@@ -3,8 +3,3 @@ export enum PaginatorTypes {
 	Single,
 	Columns,
 };
-
-export enum ComponentTypes {
-	Button,
-	SelectMenu,
-};
